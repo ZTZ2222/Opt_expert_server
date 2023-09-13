3b358afb064035ce1eda5304cae3746392f14602
ZTZ <tzamirov@gmail.com>
2023-09-13 15:10:40 +0600

    revised utils.py made dynamic urls

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import base64
+import os
 import uuid
 from passlib.context import CryptContext
 from datetime import datetime, timedelta
@@ -74,15 +75,23 @@ def upload_product_images(images: list[str]) -> list[str]:
         image_data = image_data.split(',')[1]
         image_bytes = base64.b64decode(image_data)
 
-        # Specify the save path on your system
-        save_path = f"/home/ztz/Desktop/MyProjects/2023/opt_expert/frontend/public/ProductImages/{dynamic_filename}"
+        # Specify the directory where you want to save the image
+        directory = "media/ProductImages"
+
+        # Create the directory if it doesn't exist
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        # Specify the save path for the image
+        save_path = os.path.join(directory, dynamic_filename)
 
         # Save the image to the specified path
         with open(save_path, 'wb') as f:
             f.write(image_bytes)
 
         # Store the generated image filename
-        image_urls.append(f"/ProductImages/{dynamic_filename}")
+        image_urls.append(
+            f"{os.getenv('MEDIA_URL')}media/ProductImages/{dynamic_filename}")
 
     return image_urls
 
@@ -99,15 +108,22 @@ def upload_category_image(image_base64: str) -> str:
     image_base64 = image_base64.split(',')[1]
     image = base64.b64decode(image_base64)
 
-    # Specify the save path on your system
-    save_path = f"/home/ztz/Desktop/MyProjects/2023/opt_expert/frontend/public/CategoryImages/{dynamic_filename}"
+    # Specify the directory where you want to save the image
+    directory = "media/CategoryImages"
+
+    # Create the directory if it doesn't exist
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # Specify the save path for the image
+    save_path = os.path.join(directory, dynamic_filename)
 
     # Save the image to the specified path
     with open(save_path, 'wb') as f:
         f.write(image)
 
     # Store the generated image filename
-    image_url = f"/CategoryImages/{dynamic_filename}"
+    image_url = f"{os.getenv('MEDIA_URL')}media/CategoryImages/{dynamic_filename}"
 
     return image_url
 
@@ -124,14 +140,21 @@ def upload_content_image(image_base64: str) -> str:
     image_base64 = image_base64.split(',')[1]
     image = base64.b64decode(image_base64)
 
-    # Specify the save path on your system
-    save_path = f"/home/ztz/Desktop/MyProjects/2023/opt_expert/frontend/public/ContentImages/{dynamic_filename}"
+    # Specify the directory where you want to save the image
+    directory = "media/ContentImages"
+
+    # Create the directory if it doesn't exist
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # Specify the save path for the image
+    save_path = os.path.join(directory, dynamic_filename)
 
     # Save the image to the specified path
     with open(save_path, 'wb') as f:
         f.write(image)
 
     # Store the generated image filename
-    image_url = f"/ContentImages/{dynamic_filename}"
+    image_url = f"{os.getenv('MEDIA_URL')}media/ContentImages/{dynamic_filename}"
 
     return image_url
